cf459062f5e78f85079287e8009224346656d739
chienlu <chienlu@dsl-trebrasgw1-54fa40-40.dhcp.inet.fi>
2016-11-04 02:39:22 +0200

    initial

---
+++ server.R
@@ -0,0 +1,86 @@
+library(shiny)
+
+## Twitter API
+library(twitteR)
+api_key <- "b5c0z8ROGJSXhF53XkAkf2JVm"
+api_secret <- "OKEOYlcVGdckeaCALVURP9BCikR0lMPd7sYdAzOgkwPiLKKSRJ"
+access_token <- "1871917196-hzrcO6xBVKdmk7LeG0wsHq8GD0a72UkautxvCMz"
+access_token_secret <- "LY0YIbRC1KznfLcp1ZWxDF940OXYqchsfYaXHxFrncM2v"
+setup_twitter_oauth(api_key,api_secret,access_token, access_token_secret)
+
+## Word cloud
+library(tm)
+library(wordcloud)
+wordcloud.twitter<- function(tweeets, n = 1000, min.freq = 10){
+  
+  text<- sapply(tweeets, function(x) x$getText())
+  
+  corpus<- Corpus(VectorSource(text))
+  corpus <- tm_map(corpus,
+                   content_transformer(function(x) iconv(x, to='UTF-8-MAC', sub='byte')),
+                   mc.cores=1
+  )
+  corpus <- tm_map(corpus, content_transformer(tolower), mc.cores=1)
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, removeWords, stopwords("english"))
+  corpus <- tm_map(corpus, stripWhitespace)
+  
+  wordcloud(corpus, min.freq = min.freq)
+}
+
+## Topic model
+library(topicmodels)
+
+LDA.twtter<- function(tweets, k){
+  text<- sapply(tweets, function(x) x$getText())
+  
+  corpus<- Corpus(VectorSource(text))
+  corpus <- tm_map(corpus,
+                   content_transformer(function(x) iconv(x, to='UTF-8-MAC', sub='byte')),
+                   mc.cores=1
+  )
+  corpus <- tm_map(corpus, content_transformer(tolower), mc.cores=1)
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, removeWords, stopwords("english"))
+  corpus <- tm_map(corpus, stripWhitespace)
+  
+  dtm<- DocumentTermMatrix(corpus)
+  
+  LDA(dtm, k = k, method = 'Gibbs')
+}
+
+## Server
+shinyServer(
+  function(input, output) {
+    v <- reactiveValues(tweets = NULL)
+    
+    observeEvent(input$search, {
+      withProgress(message = 'Get tweets from Twitter', {
+        v$tweets <- searchTwitter(input$key_word, n = input$numOfTweetes)
+      })
+    })
+    
+    output$hist <- renderPlot({
+      if (is.null(v$tweets)) return()
+      hist(sapply(v$tweets, function(x) x$getRetweetCount()), 
+           main = NULL,
+           xlab = "Retweet Count",
+           col = "grey")
+    })
+    
+    output$word_map <- renderPlot({
+      if (is.null(v$tweets)) return()
+      withProgress(message = "wordcloud", {
+        wordcloud.twitter(v$tweets)
+      })
+    })
+    
+    output$topics<- renderPrint({
+      if (is.null(v$tweets)) return()
+      withProgress(message = "Topic modeling", {
+        terms(LDA.twtter(v$tweets, input$numOfTopics), input$numOfTopTerms)
+      })
+    })
+  }
+  
+)
---
+++ ui.R
@@ -0,0 +1,42 @@
+library(shiny)
+shinyUI(
+  fluidPage(
+    titlePanel("Interactive Twitter Analytics"),
+    
+    sidebarLayout(
+      sidebarPanel(
+        textInput(inputId = "key_word", 
+                  label = "Enter your key words", 
+                  value = "Search Twitter"
+        ),
+        
+        sliderInput("numOfTweetes",
+                    "Number of tweets",
+                    min = 0,  max = 10000, value = 1000),
+        
+        actionButton("search", label = "Get Tweets!", icon = icon("search", lib = "font-awesome")),
+        
+        hr(),
+        
+        sliderInput("numOfTopics",
+                    "Number of topics",
+                    min = 0,  max = 20, value = 5),
+        
+        sliderInput("numOfTopTerms",
+                    "Number of top terms",
+                    min = 5,  max = 20, value = 6)
+      ),
+      
+      mainPanel(
+        tabsetPanel(
+          tabPanel("Wordcloud", plotOutput(outputId = "word_map", height = "500px")),
+          tabPanel("Histogram of Retweet Count", plotOutput(outputId = "hist", height = "500px")),
+          tabPanel("Topics Analysis", verbatimTextOutput('topics'))
+        )
+      )
+    )
+  )
+  
+)
+
+
